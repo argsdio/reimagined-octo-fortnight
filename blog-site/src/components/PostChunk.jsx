19f3292b281d05390361861af89dee018c722432
argsdio <arianneguisadio@gmail.com>
2025-05-12 17:22:35 -0400

    created folder to store React components + made an example

---
+++ blog-site/src/components/PostChunk.jsx
@@ -0,0 +1,12 @@
+function PostChunk() {
+
+    return (
+        <>
+        <div>
+            stuff in a post is supposed to go here. i have a specific layout i'm thinking of but need to draw it out...
+        </div>
+        </>
+    )
+}
+
+export default App
