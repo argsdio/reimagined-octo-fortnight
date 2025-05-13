615beb757b4169ecf60dd5f0e887301c85166bc3
argsdio <arianneguisadio@gmail.com>
2025-05-13 10:12:35 -0400

    implemented PostChunk component

--- blog-site/src/App.jsx
+++ blog-site/src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import PostChunk from './components/PostChunk.jsx'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+
+
+        {/*
+        ermm basically the idea is that everytime a new submission is made to the form function, it will create a new PostChunk object and add it to an array of PostChunk objects. then from there I can do array.map() or something to render them all on the homepage...!!??? and then i'll wrap them in a div so that they're evenly spaced no matter how many you put... T_T
+        */}
+        <PostChunk title="these won't vertically align properly :(" body={lorem} date="5/13/25" name="@arianne"/>
+
+
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
